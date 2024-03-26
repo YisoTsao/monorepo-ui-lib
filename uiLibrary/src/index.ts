199079d0b5fbeeaf5cb0c969195fa499d11ee1f6
garytsao525 <garytsao@kingsinfo.com.tw>
2024-03-26 16:52:34 +0800

    fix shadcn ui css issue

--- uiLibrary/src/index.ts
+++ uiLibrary/src/index.ts
@@ -1,7 +1,8 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+import './style.css';
 
 export * from './lib/uiLibrary';
 export * from './lib/Spin';
 export * from './lib/DropImage';
 export * from './lib/UploadImage/CustomUpload';
-export * from './components/ui/switch';
+export * from './lib/Switch';
